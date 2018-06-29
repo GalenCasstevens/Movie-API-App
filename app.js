2d9476f88a0ce55c1b4d6ce1a3bfea2b26f0c06b
Galen Casstevens <galencasstevens@gmail.com>
2018-06-29 04:49:14 +0000

    finished functionality of movie app api

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var app = express();
-var request = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
    res.render("home"); 
+});
+
+app.get("/results", function(req, res) {
+    var search = req.query.search;
+    console.log(search);
+    
+    request("http://omdbapi.com/?s=" + search + "&apikey=thewdb", function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+           var data = JSON.parse(body);
+           res.render("results", {data: data});
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Starting server..."); 
 });
